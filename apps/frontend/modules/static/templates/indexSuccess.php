3ad7c0efa09cc2be09fe78c0d5085f420a9c55b6
Rodrigo Campos H <mr.chaskas@gmail.com>
2011-08-25 14:02:41 -0300

    Listo modulo noticia (frontend/admin)

--- apps/frontend/modules/static/templates/indexSuccess.php
+++ apps/frontend/modules/static/templates/indexSuccess.php
@@ -1,25 +1,15 @@
 <div id="two-col" class="border">
   <div id="left">
     <?php $nnews = 1; ?>
+    <div id="blog">
     <?php foreach($noticias as $noticia):?>
-      <?php if($nnews==1) : ?>
-      <h1><?php echo $noticia->getTitulo();?></h1>
-      <p class="intro">
-        <?php echo $noticia->getRawValue()->getCuerpo(); ?>
-      </p>
-      <?php elseif($nnews%2==0): ?>
-      <div class="left-col">
-        <h3><?php echo $noticia->getTitulo();?></h3>
-        <?php echo $noticia->getRawValue()->getCuerpo(); ?>
+      <div class="post">
+      <h2><?php echo $noticia->getTitulo();?></h2>
+      <span class="details">Publicado el <?php echo tools::formatDate($noticia->getCreatedAt()); ?> &nbsp; | &nbsp; por <?php echo $noticia->getCreator(); ?></span>
+      <?php echo $noticia->getRawValue()->getCuerpo(); ?>
       </div>
-      <?php elseif($nnews%2!=0): ?>
-      <div class="right-col">
-        <h3><?php echo $noticia->getTitulo();?></h3>
-        <?php echo $noticia->getRawValue()->getCuerpo(); ?>
-      </div>
-      <?php endif; ?>
-      <?php $nnews++;?>
       <?php endforeach; ?>
+    </div>
   </div>
   <div id="right">
     <div class="box2">
