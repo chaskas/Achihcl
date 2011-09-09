1af7bfd305f1087f6f700876e46f3dd2191d1510
Rodrigo Campos H <mr.chaskas@gmail.com>
2011-09-09 15:52:23 -0300

    agregado algunos slides inicio

--- apps/frontend/modules/static/templates/indexSuccess.php
+++ apps/frontend/modules/static/templates/indexSuccess.php
@@ -22,10 +22,64 @@
         </div>
       </li>
       <li>
-        <img src="images/slide2.jpg" alt="" />
+        <img src="images/slide8.jpg" alt="" />
+        <div class="info">
+          <div>
+              <h1>Directiva ACHIH</h1>
+              <p>Ernesto Torres<br/>Presidente</p>
+              <p>Luis L&oacute;pez<br/>Tesorero</p>
+              <p>Irene Nuñez<br/>Viceresidenta</p>
+              <p>Yanko Galilea<br/>Secretario</p>
+          </div>
+        </div>
       </li>
       <li>
-        <img src="images/slide5.jpg" alt="" />
+        <img src="images/slide6.jpg" alt="" />
+        <div class="info">
+          <div>
+            <h1>Comisi&oacute;n de Capacitaci&oacute;n</h1>
+            <p>Mauricio Rivera<br/>Hospital Cl&iacute;nico U. de Chile</p><p>Irene Nuñez<br/>Vicepresidenta ACHIH, HUAP</p><p>Esteban Pino<br/>Jefe Carrera Ing. Civil Biom&eacute;dica <br/>U. de Concepci&oacute;n</p>
+          </div>
+        </div>
+      </li>
+      <li>
+        <img src="images/slide11.jpg" alt="" />
+        <div class="info">
+          <div>
+              <h1>Comisi&oacute;n de Climatizaci&oacute;n</h1>
+              <p>Nelson Gonzalez<br/>Hospital Cl&iacute;nico U. de Chile</p>
+              <p>Joaquin Fort<br/>EMTE HVAC</p>
+          </div>
+        </div>
+      </li>
+      <li>
+        <img src="images/slide13.jpg" alt="" />
+        <div class="info">
+          <div>
+              <h1>Comisi&oacute;n de Agua Potable</h1>
+              <p>Jos&eacute; Bertulini<br/>SS Valdivia</p>
+          </div>
+        </div>
+      </li>
+      <li>
+        <img src="images/slide10.jpg" alt="" />
+        <div class="info">
+          <div>
+              <h1>Comisi&oacute;n Infraestructura</h1>
+          </div>
+        </div>
+      </li>
+      <li>
+        <img src="images/slide9.jpg" alt="" />
+        <div class="info">
+          <div>
+              <h1>Comisi&oacute;n T&eacute;cnica</h1>
+              <p>Nancy Fernandez<br/>MINSAL</p>
+              <p>Sergio Barriga<br/>SS OHiggins</p>
+              <p>Judith Moreno<br/>SS OHiggins</p>
+              <p>Regina Barra<br/>SS Valdivia</p>
+          </div>
+        </div>
       </li>
     </ul>
   </div>
@@ -37,7 +91,7 @@
       <?php foreach ($noticias as $noticia): ?>
         <div class="post">
           <h2><?php echo link_to($noticia->getTitulo(), 'noticia/show?id=' . $noticia->getId()); ?></h2>
-          <span class="details">Publicado el <?php echo tools::formatDate($noticia->getCreatedAt()); ?> &nbsp; | &nbsp; por <?php echo $noticia->getCreator(); ?></span>
+          
           <?php echo tools::getResume($noticia->getRawValue()->getCuerpo()); ?>
           <br/>
           <a class="more" href="<?php echo url_for('noticia/show?id=' . $noticia->getId()); ?>">Leer m&aacute;s</a>
